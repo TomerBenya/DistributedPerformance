2e2f089c81e6396d8ed2ac6eb87ee98dafb92ce9
TomerBenya <tomerbenyaakov@gmail.com>
2020-12-01 12:40:09 -0500

    Create PerfScript.js
    basic node.script which reads in amplitude values from a synth and broadcasts them to shanes server.

---
+++ PerfScript.js
@@ -0,0 +1,16 @@
+const maxApi = require('max-api');
+const io = require('socket.io-client');
+const socket = io('http://h.s2.cloud.shaneth.com');
+
+
+var x = 0;
+
+maxApi.addHandler('bang', () => {
+		x=Math.random();
+		maxApi.outlet(x);
+		
+});
+maxApi.addHandler("echo", (msg) => {
+	socket.emit('value', msg);
+	maxApi.post(msg);
+});
