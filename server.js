444f0f35c21f69150ba611a15b2af03c3956d001
Shaneth <shanethp@gmail.com>
2020-12-01 13:07:21 -0500

    oops

--- server.js
+++ server.js
@@ -4,15 +4,22 @@
 const httpServer = require("http").createServer();
 const io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
 io.on("connection", (socket) => {
    io.emit("hello", "world");
 });
 
 io.on('connection', function(socket){
   socket.on('value', function(msg){
-    io.emit('value', msg);
+      io.emit('value', msg);
+      console.log(msg);
   });
 });
+
 
 /*clients will recieve msg variable passed to them add this to your client side script:
 
